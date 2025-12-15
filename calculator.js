e888cc32cddcade88c4fa1d28efd5002b92ca48c
Rudolf Schlenger <schlengerrudolfde@gmail.com>
2025-12-15 12:11:24 +0100

    Add operate function

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+let numOne = 0;
+let numTwo = 0;
+let op = "";
+
 function add(one, two) {
     return one + two;
 }
@@ -12,4 +16,13 @@ function multiply(one, two) {
 
 function divide(one, two) {
     return one / two;
+}
+
+function operator(op, one, two) {
+    switch(op) {
+        case "+": return add(one, two);
+        case "-": return subtract(one, two);
+        case "*": return multiply(one, two);
+        case "/": return divide(one, two);
+    }
 }
